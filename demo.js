b86b9da4d09c46fba7c019f0e31c01fe6dd4f710
zouzhuo <zkaggle@163.com>
2024-07-05 19:11:51 +0800

    第一次上传文件

---
+++ demo.js
@@ -0,0 +1,4 @@
+window.onload = function (){
+    let box = document.getElementsByClassName("box")[0]
+    box.addEventListener("click",() =>{alert("这是测试代码")})
+}
---
+++ test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/7/5 下午6:52
+# @Author  : Jellison
+# @Email   : zkaggle@163.com
+# @File    : test.py
+# @Software: PyCharm
+"""这是测试代码"""
